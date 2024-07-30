c0c78d2fe4b5c76d7ce775eb56517fbe45b62f6e
DrewDeMo <drdemaiolo@gmail.com>
2024-07-30 15:09:11 -0400

    Test.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,9 @@
 // File: /finance/src/components/Navigation.js
+// File: /finance/src/components/Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
+import { ReactComponent as DemaioloLogo } from '../assets/images/d_logo.svg'; // Import the SVG as a React component
 
 const Navigation = () => {
     const handleLogout = async () => {
@@ -12,6 +14,7 @@ const Navigation = () => {
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="flex items-center">
+                    <DemaioloLogo className="icon w-8 h-8 mr-2" /> {/* Use the SVG component here */}
                     <span className="text-white font-bold text-xl">FinanceApp</span>
                 </Link>
                 <div className="flex items-center">
@@ -29,3 +32,4 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
